4a717ba7f30c263b03f0f9800e91b1522a21c4b1
Ethan Bielecki <ebielecki2003@gmail.com>
2025-04-04 00:39:23 -0400

    Style modification of code

--- Labs/SpookyForest/D4.js
+++ Labs/SpookyForest/D4.js
@@ -1,64 +1,61 @@
-class D4 extends GameObject
-{
-	constructor()
-	{
+class D4 extends GameObject {
+	constructor() {
 		super();
+		this.loc = [0.0, 0.0, 0.0];
+		this.rot = [0.0, 0.0, 0.0];
+
 		this.isTrigger = true;
-		this.buffer=gl.createBuffer();
-		 gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
-		 
+		this.buffer = gl.createBuffer();
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
+		
 		 //Now we want to add color to our vertices information.
-		 this.vertices =
-		 [	
-		 -.5,-.5,0,0,0,0,
-		 .5,-.5,0,1,0,0,
-		 0,.5,0,1,0,0,
-		 
-		 -.5,-.5,0,0,1,0,
-		 0,0,-.5,0,1,0,
-		 .5,-.5,0,0,1,0,
-		 
-		 0,0,-.5,0,0,1,
-		 .5,-.5,0,0,0,1,
-		 0,.5,0,0,0,1,
-		 
-		 0,.5,0,1,1,0,
-		 0,0,-.5,1,1,0,
-		 -.5,-.5,0,1,1,0
+		this.vertices = [	
+            -.5,-.5,0,0,0,0,
+            .5,-.5,0,1,0,0,
+            0,.5,0,1,0,0,
+            
+            -.5,-.5,0,0,1,0,
+            0,0,-.5,0,1,0,
+            .5,-.5,0,0,1,0,
+            
+            0,0,-.5,0,0,1,
+            .5,-.5,0,0,0,1,
+            0,.5,0,0,0,1,
+            
+            0,.5,0,1,1,0,
+            0,0,-.5,1,1,0,
+            -.5,-.5,0,1,1,0
 		];
+
 		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.vertices), gl.STATIC_DRAW);
-		this.loc = [0.0,0.0,0.0];
-		this.rot = [0.0,0.0,0.0];
-		this.angVelocity = [0,.01,0];
+		this.angVelocity = [0, 0.01, 0];
 	}
-	Render(program)
-	{
-		//First we bind the buffer for triangle 1
+
+	Render(program) {
 		var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
 		gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
-		var size = 3;          // 2 components per iteration
-		var type = gl.FLOAT;   // the data is 32bit floats
-		var normalize = false; // don't normalize the data
-		var stride = 6*Float32Array.BYTES_PER_ELEMENT;	//Size in bytes of each element     // 0 = move forward size * sizeof(type) each iteration to get the next position
-		var offset = 0;        // start at the beginning of the buffer
+		var size = 3;
+		var type = gl.FLOAT;
+		var normalize = false;
+		var stride = 6*Float32Array.BYTES_PER_ELEMENT;
+		var offset = 0;
 		gl.enableVertexAttribArray(positionAttributeLocation);
 		gl.vertexAttribPointer(positionAttributeLocation, size, type, normalize, stride, offset);
 			
 		
 		//Now we have to do this for color
-		var colorAttributeLocation = gl.getAttribLocation(program,"vert_color");
-		//We don't have to bind because we already have the correct buffer bound.
+		var colorAttributeLocation = gl.getAttribLocation(program, "vert_color");
 		size = 3;
 		type = gl.FLOAT;
 		normalize = false;
-		stride = 6*Float32Array.BYTES_PER_ELEMENT;	//Size in bytes of each element
-		offset = 3*Float32Array.BYTES_PER_ELEMENT;									//size of the offset
+		stride = 6*Float32Array.BYTES_PER_ELEMENT;
+		offset = 3*Float32Array.BYTES_PER_ELEMENT;
 		gl.enableVertexAttribArray(colorAttributeLocation);
 		gl.vertexAttribPointer(colorAttributeLocation, size, type, normalize, stride, offset);
 				
-		var tranLoc  = gl.getUniformLocation(program,'transform');
+		var tranLoc  = gl.getUniformLocation(program, 'transform');
 		gl.uniform3fv(tranLoc,new Float32Array(this.loc));
-		var thetaLoc = gl.getUniformLocation(program,'rotation');
+		var thetaLoc = gl.getUniformLocation(program, 'rotation');
 		gl.uniform3fv(thetaLoc,new Float32Array(this.rot));
 		
 		
@@ -67,8 +64,7 @@ class D4 extends GameObject
 		var count = 12;
 		gl.drawArrays(primitiveType, offset, count);
 	}
-	Update()
-	{
+	Update() {
 		this.Move();
 	}
 	
